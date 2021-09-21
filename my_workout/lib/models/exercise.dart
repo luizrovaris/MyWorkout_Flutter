c7062ac64e4fdf2e4f9efb4af42804335261a827
luizrovaris <luiz_rovaris@hotmail.com>
2021-09-21 17:49:25 +0100

    Adding models

---
+++ my_workout/lib/models/exercise.dart
@@ -0,0 +1,9 @@
+class Exercise{
+  late String id;
+  late String name;
+  late String description;
+  late String imageUrl;
+  late String workOutId;
+
+  Exercise(this.id, this.name, this.description, this.imageUrl, this.workOutId);
+}
---
+++ my_workout/lib/models/workout.dart
@@ -0,0 +1,8 @@
+class Workout{
+  late String id;
+  late String name;
+  late String image;
+  late int weekDay;
+
+  Workout(this.id, this.name, this.image, this.weekDay);
+}
