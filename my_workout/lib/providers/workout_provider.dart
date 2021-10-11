cc4ad05addf65635d4afbd2790ce3c08f52ef391
luizrovaris <luiz_rovaris@hotmail.com>
2021-10-11 21:40:02 +0100

    Getting workout from firebase

--- my_workout/lib/providers/workout_provider.dart
+++ my_workout/lib/providers/workout_provider.dart
@@ -5,9 +5,20 @@ import '../models/workout.dart';
 
 class WorkoutProvider with ChangeNotifier {
   List<Workout> _workouts = [];
+  final String baseUrl =
+      'https://workoutapp-dfe56-default-rtdb.europe-west1.firebasedatabase.app/workout.json';
 
   Future<List<Workout>> get() async {
-    return await Future.delayed(Duration(seconds: 3), () => [..._workouts]);
+    //return await Future.delayed(Duration(seconds: 3), () => [..._workouts]);
+    List<Workout> workouts = [];
+    final response = await http.get(Uri.parse(this.baseUrl));
+    final decoded = json.decode(response.body) as Map<String, dynamic>;
+    decoded.forEach((key, value) {
+      workouts
+          .add(Workout(key, value['name'], value['image'], value['weekDay']));
+    });
+
+    return workouts;
   }
 
   Workout getById(String id) {
@@ -17,11 +28,11 @@ class WorkoutProvider with ChangeNotifier {
   Future<void> add(Workout workout) async {
     print('VAI SALVAR');
     final response = await http.post(
-      Uri.parse('https://workoutapp-dfe56-default-rtdb.europe-west1.firebasedatabase.app/workout.json'),
+      Uri.parse(this.baseUrl),
       body: json.encode(
         {
           'name': workout.name,
-          'imageUrl': workout.image,
+          'image': workout.image,
           'weekDay': workout.weekDay
         },
       ),
@@ -29,7 +40,7 @@ class WorkoutProvider with ChangeNotifier {
 
     print(response.statusCode);
     print(response.body);
-    
+
     notifyListeners();
   }
 
