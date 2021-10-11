3fa1c57b62e0b7911bd0af8d3b0e547f642f5dca
luizrovaris <luiz_rovaris@hotmail.com>
2021-10-11 21:50:53 +0100

    Updating workout on firebase

--- my_workout/lib/providers/workout_provider.dart
+++ my_workout/lib/providers/workout_provider.dart
@@ -6,19 +6,19 @@ import '../models/workout.dart';
 class WorkoutProvider with ChangeNotifier {
   List<Workout> _workouts = [];
   final String baseUrl =
-      'https://workoutapp-dfe56-default-rtdb.europe-west1.firebasedatabase.app/workout.json';
+      'https://workoutapp-dfe56-default-rtdb.europe-west1.firebasedatabase.app/workout';
 
   Future<List<Workout>> get() async {
     //return await Future.delayed(Duration(seconds: 3), () => [..._workouts]);
-    List<Workout> workouts = [];
-    final response = await http.get(Uri.parse(this.baseUrl));
+    _workouts = [];
+    final response = await http.get(Uri.parse('$baseUrl.json'));
     final decoded = json.decode(response.body) as Map<String, dynamic>;
     decoded.forEach((key, value) {
-      workouts
+      _workouts
           .add(Workout(key, value['name'], value['image'], value['weekDay']));
     });
 
-    return workouts;
+    return _workouts;
   }
 
   Workout getById(String id) {
@@ -26,9 +26,8 @@ class WorkoutProvider with ChangeNotifier {
   }
 
   Future<void> add(Workout workout) async {
-    print('VAI SALVAR');
     final response = await http.post(
-      Uri.parse(this.baseUrl),
+      Uri.parse('$baseUrl.json'),
       body: json.encode(
         {
           'name': workout.name,
@@ -45,14 +44,25 @@ class WorkoutProvider with ChangeNotifier {
   }
 
   Future<void> update(Workout workout) async {
-    _workouts = _workouts.map((e) {
-      if (e.id == workout.id) {
-        e.name = workout.name;
-        e.image = workout.image;
-        e.weekDay = workout.weekDay;
-      }
-      return e;
-    }).toList();
+    var response = await http.put(
+      Uri.parse('$baseUrl/${workout.id}.json'),
+      body: json.encode({
+        'name': workout.name,
+        'image': workout.image,
+        'weekDay': workout.weekDay
+      }),
+    );
+    print(response.statusCode);
+
+    // _workouts = _workouts.map((e) {
+    //   if (e.id == workout.id) {
+    //     e.name = workout.name;
+    //     e.image = workout.image;
+    //     e.weekDay = workout.weekDay;
+    //   }
+    //   return e;
+    // }).toList();
+    
     notifyListeners();
   }
 
