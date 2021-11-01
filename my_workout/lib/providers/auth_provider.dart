9d7459af50984769295c16f8110b628c3d62f042
luizrovaris <luiz_rovaris@hotmail.com>
2021-11-01 22:49:55 +0000

    Adding auth provider

---
+++ my_workout/lib/providers/auth_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AuthProvider with ChangeNotifier {
+  final String baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts';
+  final String key = 'AIzaSyCu9vXksv4l23DuUXS4NEQsAyJdnUCkdzI';
+
+  Future<void> manageAuth(email, password, action) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl:$action?key=[$key]'),
+        body: jsonEncode({
+          'email': email,
+          'password': password
+        }),
+      );
+
+      print(response.statusCode);
+      print(response.body);
+      
+      notifyListeners();
+    } catch (e) {
+      throw (e as FormatException).message;
+    }
+  }
+}
